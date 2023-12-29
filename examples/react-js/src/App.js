9513ba2ffd8e017177dbb9991536ff6e590c7b6f
y-solb <ysolb99@gmail.com>
2023-12-29 16:17:06 +0900

    Feat: add react-js example

--- examples/react-js/src/App.js
+++ examples/react-js/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>bottom-sheet demo</h1>
+      <button
+        type="button"
+        onClick={() => document.getElementById('testBottomSheet').openSheet()}
+      >
+        openSheet
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          document.getElementById('testBottomSheet').openFullSheet()
+        }
+      >
+        openFullSheet
+      </button>
+
+      <bottom-sheet id="testBottomSheet" title="Title is Lorem ipsum">
+        <main className="_example">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div>
+            <button
+              type="button"
+              onClick={() =>
+                document.getElementById('testBottomSheet').closeSheet()
+              }
+            >
+              closeSheet
+            </button>
+            <button
+              type="button"
+              onClick={() =>
+                document.getElementById('testBottomSheet').fullSheet()
+              }
+            >
+              fullSheet
+            </button>
+          </div>
+        </main>
+      </bottom-sheet>
     </div>
   );
 }

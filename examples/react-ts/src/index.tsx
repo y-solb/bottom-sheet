fe8c4390da015b28b11c09324c4572ab62e6ed3f
y-solb <ysolb99@gmail.com>
2023-12-29 17:08:45 +0900

    Feat: add react-ts example

--- examples/react-ts/src/index.tsx
+++ examples/react-ts/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@solb/bottom-sheet';
+import '@solb/bottom-sheet/style/style.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- examples/react-ts/src/react-app-env.d.ts
+++ examples/react-ts/src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+declare namespace JSX {
+  interface IntrinsicElements {
+    ['bottom-sheet']: React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
+  }
+}
